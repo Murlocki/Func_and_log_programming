30cc0287985166e7d70dd98e23d8eb4a53b648c4
Murlocki <kirillgod6@gmail.com>
2024-02-18 16:56:47 +0300

    3.3.(custom predicate) Add predicates nephew(PossibleNephew, PossibleAU), nephews(InputPerson)

--- lab1/tasks 2-4/tasks2-4.pl
+++ lab1/tasks 2-4/tasks2-4.pl
@@ -103,3 +103,11 @@ siblings(PossibleSib1,PossibleSib2):- (PossibleSib1 \== PossibleSib2),parent(Com
 % nibling(+PossibleNib: atom, PossibleAU: atom) is det
 % True if PossibleNib is a nibling of PossibleAU
 nibling(PossibleNib,PossibleAU):- parent(NibParent,PossibleNib),siblings(NibParent,PossibleAU).
+
+% nephew(+PossibleNephew: atom, PossibleAU:atom) is det
+% True if PossibleNephew is a nephew of PossibleAU
+nephew(PossibleNephew,PossibleAU):- man(PossibleNephew),nibling(PossibleNephew,PossibleAU).
+
+% nephews(+InputPerson: atom) is failure
+% Print all nephews of InputPerson
+nephews(InputPerson):- nephew(PossibleNephew,InputPerson),print(PossibleNephew),nl,fail.
