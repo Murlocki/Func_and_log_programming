377fe05734005c530585e43bd617b8fa2d9dd182
Murlocki <131604158+Murlocki@users.noreply.github.com>
2024-02-22 15:01:30 +0300

    Create problem_acinator file and move acinator from oldrus_family to it

---
+++ lab1/problem_acinator(task 4)/problem_acinator.pl
@@ -0,0 +1,152 @@
+% Task 4
+main :-
+    retractall(asked(_,_)),
+    fault(Problem),
+    !,
+    nl,
+    write('The problem is '), write(Problem), write(.), nl.
+main :-
+    nl,
+    write('The problem cannot be recognized.'), nl.
+
+problem(disc_format) :-
+    query('Does the computer show error cannot format').
+
+problem(boot_failure) :-
+    query('Does the computer show boot failure').
+
+problem(bad_sector) :-
+    query('Does the computer show bad sector error').
+
+problem(cannot_read) :-
+    query('Does the computer show cannot read from specified device').
+
+problem(long_beep) :-
+    query('Is there a long beep during bootup').
+
+problem(short_beep) :-
+    query('Is there a short beep during bootup').
+
+problem(two_long_beeps) :-
+    query('Are there two long beeps during bootup').
+
+problem(two_short_beeps) :-
+    query('Are there two short beeps during bootup').
+
+problem(blank_display) :-
+    query('Is there a blank display during bootup').
+
+problem(repeating_short_beeps) :-
+    query('Are there repeating short beeps during bootup').
+
+problem(continuous_beeps) :-
+    query('Is there a continuous beep during bootup').
+
+problem(no_beep) :-
+    query('Is there a beep during bootup').
+
+problem(not_printing) :-
+    query('Is there a problem with printing').
+
+problem(missing_dots) :-
+    query('Is there a missing character during printing').
+
+problem(non_uniform_printing) :-
+    query('Is there uniform printing').
+
+problem(spread_ink) :-
+    query('Is there spreading of ink during printing').
+
+problem(paper_jam) :-
+    query('Is there a paper jam during printing').
+
+problem(out_of_paper) :-
+    query('Is there out-of- paper error during printing').
+% New problems
+problem(error_screen):-query('Is there an error screen during bootup').
+problem(resolution_problem):- query('Is there broken resolution or your screen').
+problem(keyboard_input):-query('Does computer react on input from your working keyboard ').
+problem(usb_connection):-query('Is your device appear in menu after using of usb connection').
+problem(wifi_adapter):-query('Does your computer see any wifi network').
+% new objects with extra questions
+fault(easy_video_card_connection):-
+    (   problem(resolution_problem);
+    problem(blank_display)).
+fault(extra_bios_problem):-
+    problem(resolution_problem),
+    problem(keyboatd_input),
+    problem(boot_failure).
+fault(keyboard):-
+    problem(cannot_read),
+    problem(not_printin),
+    problem(missing_dots),
+    problem(usb_connections).
+fault(wifi_adapter):-problem(wifi_adapter).
+
+
+
+% new objects without extra questions
+fault(video_card_connection):-
+    problem(blank_display).
+
+fault(bios_problem):-
+    problem(boot_failure).
+fault(keyboard_connection):-
+    problem(cannot_read),
+    problem(not_printin),
+    problem(missing_dots)
+.
+% Old objects
+fault(power_supply) :-
+    problem(repeating_short_beeps),
+    problem(continuous_beeps),
+    problem(blank_display),
+    problem(no_beep).
+
+fault(display_adapter) :-
+    problem(long_beep),
+    problem(two_short_beeps),
+    problem(blank_display),
+    problem(no_beep).
+
+fault(motherboard) :-
+    problem(long_beep),
+    problem(short_beep).
+
+fault(hard_disc) :-
+    problem(two_short_beeps),
+    problem(blank_display).
+
+fault(booting_problem) :-
+    problem(bad_sector),
+    problem(boot_failure).
+
+fault(floppy_disk_unusable) :-
+    problem(bad_sector),
+    problem(cannot_read),
+    problem(disc_format).
+
+fault(printer_head) :-
+    problem(not_printing),
+    problem(missing_dots),
+    problem(nonuniform_printing).
+
+fault(ribbon) :-
+    problem(not_printing),
+    problem(missing_dots),
+    problem(spread_ink).
+
+fault(paper) :-
+    problem(not_printing),
+    problem(paper_jam),
+    problem(out_of_paper).
+
+
+query(Prompt) :-
+    (   asked(Prompt, Reply) -> true
+    ;   nl, write(Prompt), write(' (y/n)? '),
+        read(X),(X = y -> Reply = y ; Reply = n),
+	assert(asked(Prompt, Reply))
+    ),
+    Reply = y.
+
