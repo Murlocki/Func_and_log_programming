39377fab38b6c5bf38145e94014ec70aa584f315
Murlocki <kirillgod6@gmail.com>
2024-03-01 23:38:20 +0300

    2.1. Add max_digit_call(InputNumber,MaximumNumber), max_digit_down(CurrentNumber,CurrentMaximumDigit,ResultMaximumDigit) predicates

--- lab2/number_predicates(task2)/number_predicates.pl
+++ lab2/number_predicates(task2)/number_predicates.pl
@@ -7,5 +7,13 @@ max(_,SecondNumber,SecondNumber).
 
 % max_digit_up(+InputNumber:integer,?MaximumNumber:integer) is det
 max_digit_up(0,0):-!.
-max_digit_up(InputNumber,CurrentMaximumNumber):- NextInputNumber is InputNumber div 10, max_digit_up(NextInputNumber,CurrentResult),  NewDigit is InputNumber mod 10, max(NewDigit,CurrentResult,NewResult), CurrentMaximumNumber is NewResult.
+max_digit_up(InputNumber,CurrentMaximumDigit):- NextInputNumber is InputNumber div 10, max_digit_up(NextInputNumber,CurrentResult),  NewDigit is InputNumber mod 10, max(NewDigit,CurrentResult,NewResult), CurrentMaximumDigit is NewResult.
+
+
+% max_digit_call(+InputNumber:integer,?MaximumNumber:integer) is det
+max_digit_call(InputNumber,MaximumNumber):-max_digit_down(InputNumber,0,MaximumNumber),!. 
+
+% max_digit_down(+CurrentNumber:integer, +CurrentMaximumDigit:integer,+ResultMaximumDigit) is det
+max_digit_down(0,ResultMaximumDigit,ResultMaximumDigit):-!.
+max_digit_down(CurrentNumber,CurrentMaximumDigit,ResultMaximumDigit):- NewDigit is CurrentNumber mod 10, max(CurrentMaximumDigit,NewDigit,NewResultDigit), NewNumber is CurrentNumber div 10, max_digit_down(NewNumber,NewResultDigit,ResultMaximumDigit).
 
