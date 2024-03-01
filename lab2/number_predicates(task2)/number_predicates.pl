dc893d354c07cfd32c75d00fbd869f2be4dbc000
Murlocki <kirillgod6@gmail.com>
2024-03-01 23:49:19 +0300

    2.2. Add multiple_of_three(InputNumber,Result),mult_of_3_sum_up(InputNumber,ResultSum) predicates

--- lab2/number_predicates(task2)/number_predicates.pl
+++ lab2/number_predicates(task2)/number_predicates.pl
@@ -1,19 +1,28 @@
 % task 2
 
 %2.1
-% max(+FirstNumber:integer,+SecondNumber:integer,?MaximumNumber:integer) is det
+% max(+FirstNumber:integer,+SecondNumber:integer,?MaximumNumber:integer)
 max(FirstNumber,SecondNumber,FirstNumber):- FirstNumber>SecondNumber,!.
 max(_,SecondNumber,SecondNumber).
 
-% max_digit_up(+InputNumber:integer,?MaximumNumber:integer) is det
+% max_digit_up(+InputNumber:integer,?MaximumNumber:integer)
 max_digit_up(0,0):-!.
 max_digit_up(InputNumber,CurrentMaximumDigit):- NextInputNumber is InputNumber div 10, max_digit_up(NextInputNumber,CurrentResult),  NewDigit is InputNumber mod 10, max(NewDigit,CurrentResult,NewResult), CurrentMaximumDigit is NewResult.
 
 
-% max_digit_call(+InputNumber:integer,?MaximumNumber:integer) is det
+% max_digit_call(+InputNumber:integer,?MaximumNumber:integer)
 max_digit_call(InputNumber,MaximumNumber):-max_digit_down(InputNumber,0,MaximumNumber),!. 
 
-% max_digit_down(+CurrentNumber:integer, +CurrentMaximumDigit:integer,+ResultMaximumDigit) is det
+% max_digit_down(+CurrentNumber:integer, +CurrentMaximumDigit:integer,+ResultMaximumDigit)
 max_digit_down(0,ResultMaximumDigit,ResultMaximumDigit):-!.
 max_digit_down(CurrentNumber,CurrentMaximumDigit,ResultMaximumDigit):- NewDigit is CurrentNumber mod 10, max(CurrentMaximumDigit,NewDigit,NewResultDigit), NewNumber is CurrentNumber div 10, max_digit_down(NewNumber,NewResultDigit,ResultMaximumDigit).
 
+
+%2.2
+% multiplt_of_three(+InputNumber:integer, ?Result:Integer)
+multiplt_of_three(InputNumber,Result):-0 is InputNumber mod 3, Result is 1,!.
+multiplt_of_three(_,Result):-Result is 0,!.
+
+%mult_of_3_sum_up(+InputNumber:integer,?ResultSum:integer)
+mult_of_3_sum_up(0,0):-!.
+mult_of_3_sum_up(InputNumber,ResultSum):-NewInputNumber is InputNumber div 10, mult_of_3_sum_up(NewInputNumber,PrevResult), NewDigit is InputNumber mod 10, multiplt_of_three(NewDigit,MultResult), ResultSum is PrevResult + NewDigit * MultResult.
