c9a021f24b3862169b1985ffaeffbbcb275fe264
Murlocki <kirillgod6@gmail.com>
2024-03-15 15:53:13 +0300

    Lab 3 task 1.1. Add comments

--- lab3/CombObjects/src/PlacementsWithRepeats.java
+++ lab3/CombObjects/src/PlacementsWithRepeats.java
@@ -1,14 +1,22 @@
+//Класс для вывода всех размещений с повторений для алфавита длиной n
 public class PlacementsWithRepeats {
     private char[] alphabet;
     private int n;
 
+    // Конструктор класс
+    // Принимает на вход массив символов - алфавит
     public PlacementsWithRepeats(char[] inputAlphabet){
         this.alphabet = inputAlphabet;
         this.n = inputAlphabet.length;
     }
+    // Метод для вызоыва рекурсивной функции вывода всех размещений с повторениями длины k
+    // Принимает длину размещения k
     public void printAllPlacementsWithRep(int k){
         this.printAllPlacementsWithRepRec(0,k,new char[k]);
     }
+
+    // Рекурсивный метод для вывода всех размещений с повторениями длины k
+    // Принимает текущую позицию в размещении, длину размещения, уже построенную часть размещения
     private void printAllPlacementsWithRepRec(int curPos,int k,char[]placement){
         if(curPos==k){
             for(int i=0;i<k;i++){
