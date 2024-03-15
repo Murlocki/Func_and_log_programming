8883b7d33741e2c655a0566e80a0099093c90b9a
Murlocki <kirillgod6@gmail.com>
2024-03-15 20:10:21 +0300

    Lab 3 task 1.7. Create 3-a words predicates
    Add make_pos_list(+K:integer,+CurPos:integer,-OutPut:List),make_3a_empty_word(+K:integer,+CurIndex:integer,+PositionsOfA:List,-OutputWord:List),build_word(-Word:List,+AWord:List,+RestWord:List),build_3a_words_of_k(+Alphabet:List,+K:integer,-Word:List)

--- lab3/ComObjectsProlog/CombObjects.pl
+++ lab3/ComObjectsProlog/CombObjects.pl
@@ -13,3 +13,28 @@ razm_povt(Alphabet,Ncur,RazmCur,Razm):- in_list(Alphabet,El),NNew is Ncur - 1, r
 comb(_,[],0):-!.
 comb([El|RestAlp],[El|PrevComb],K):-KNew is K - 1,comb(RestAlp,PrevComb,KNew).
 comb([El|RestAlp],PrevComb,K):- comb(RestAlp,PrevComb,K).
+
+%make_pos_list(+K:integer,+CurPos:integer,-OutPut:List)
+%Create a list with numbers from 0 to K
+make_pos_list(K,K,[]):-!.
+make_pos_list(K,CurPos,[NewPos|TailPos]):-NewPos is CurPos + 1,make_pos_list(K,NewPos,TailPos).
+
+%make_3a_empty_word(+K:integer,+CurIndex:integer,+PositionsOfA:List,-OutputWord:List)
+%Create OutputWord with length k and 'a' on positions from PositionsOfA
+make_3a_empty_word(K,K,_,[]):-!.
+make_3a_empty_word(K,CurIndex,[NewIndex|PositionsOfATail],[a|WordTail]):- NewIndex is CurIndex + 1,make_3a_empty_word(K,NewIndex,PositionsOfATail,WordTail),!.
+make_3a_empty_word(K,CurIndex,PositionsOfA,[_|Tail]):- NewIndex is CurIndex + 1, make_3a_empty_word(K,NewIndex,PositionsOfA,Tail).
+
+%build_word(-Word:List,+AWord:List,+RestWord:List)
+%Create word in Word with 3 'a'
+build_word([],[],_):-!.
+build_word([a|WordTail],[El|AwordTail],RestWord):-nonvar(El),build_word(WordTail,AwordTail,RestWord),!.
+build_word([Y|WordTail],[El|AwordTail],[Y|RestWordTail]):-var(El),build_word(WordTail,AwordTail,RestWordTail),!.
+
+
+%build_3a_words_of_k(+Alphabet:List,+K:integer,-Word:List)
+%Create a word with length k and 3 'a'
+build_3a_words_of_k(Alphabet,K,Word):-make_pos_list(K,0,PositionList),
+    comb(PositionList,PositionsOfA,3),make_3a_empty_word(K,0,PositionsOfA,OutputWord), 
+    Alphabet = [a|NewAlphabet], M is K-3, razm_povt(NewAlphabet,M,[],RestWord),
+    build_word(Word,OutputWord,RestWord).
