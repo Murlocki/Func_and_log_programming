52e5993e48918f8dbea818346209daa2fa744174
Murlocki <kirillgod6@gmail.com>
2024-03-15 18:58:20 +0300

    Lab 3 task 1.6. Add combinations without rep

--- lab3/ComObjectsProlog/CombObjects.pl
+++ lab3/ComObjectsProlog/CombObjects.pl
@@ -9,3 +9,7 @@ in_list([_|T],El):-in_list(T,El).
 razm_povt(_,0,Razm,Razm):-!.
 razm_povt(Alphabet,Ncur,RazmCur,Razm):- in_list(Alphabet,El),NNew is Ncur - 1, razm_povt(Alphabet,NNew,[El|RazmCur],Razm).
 
+%comb(+Alphabet:List,-Combination:List,+K:integer)
+comb(_,[],0):-!.
+comb([El|RestAlp],[El|PrevComb],K):-KNew is K - 1,comb(RestAlp,PrevComb,KNew).
+comb([El|RestAlp],PrevComb,K):- comb(RestAlp,PrevComb,K).
