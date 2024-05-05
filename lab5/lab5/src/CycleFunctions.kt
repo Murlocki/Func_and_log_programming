d8a39801127ac6e4a29e601b5fb07fad089a57d1
Murlocki <kirillkornilov03@gmail.com>
2024-05-05 15:46:16 +0300

    Lab 5 task 2 2.2. Add cycle function for finding sum of multiple of 3 digits

--- lab5/lab5/src/CycleFunctions.kt
+++ lab5/lab5/src/CycleFunctions.kt
@@ -1,14 +1,37 @@
+import kotlin.math.abs
 import kotlin.math.max
+import kotlin.math.round
 
 class CycleFunctions {
+
+    //Циклическая функция для превращение дробного числа в целое
+    fun shiftToInteger(n:Double): Int{
+        var result = n;
+        while(round(result)!=result){
+            result = result * 10;
+        }
+        return result.toInt()
+    }
+
     //Циклическая функция для подсчета макс цифры числа
-    fun maxdigit(n:Int): Int {
-        var currentMax:Int=n%10;
-        var currentN:Int = n/10;
+    fun maxdigit(n:Double): Int {
+        var currentN:Int = abs(shiftToInteger(n));
+        var currentMax:Int=currentN%10;
         while(currentN>0){
             currentMax = max(currentMax,currentN%10);
-            currentN = currentN/10;
+            currentN /= 10;
         }
         return currentMax;
     }
+    fun sum3Digits(n:Double):Int{
+        var currentN:Int = abs(shiftToInteger(n));
+        var digitSum=0;
+        while(currentN>0){
+            var currentDigit = currentN%10;
+            digitSum += if(currentDigit%3==0) currentDigit else 0;
+            currentN /= 10;
+        }
+        return digitSum;
+    }
+
 }
--- lab5/lab5/src/Main.kt
+++ lab5/lab5/src/Main.kt
@@ -1,4 +1,6 @@
 fun main() {
     val c = CycleFunctions();
-    println(c.maxdigit(897))
+    println(c.maxdigit(897.0))
+    println(c.sum3Digits(8976.0));
+    println(-13%10)
 }
