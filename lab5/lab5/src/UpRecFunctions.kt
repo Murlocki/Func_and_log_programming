55509a041a0b68449380ae6e839376778d4023dc
Murlocki <kirillkornilov03@gmail.com>
2024-05-08 01:39:17 +0300

    Lab 5 task 4 change rec functions

--- lab5/lab5/src/UpRecFunctions.kt
+++ lab5/lab5/src/UpRecFunctions.kt
@@ -2,14 +2,24 @@ import kotlin.math.abs
 import kotlin.math.max
 
 class UpRecFunctions {
+
+    //Функция высшего порядка для обработки цифр числа
+    fun digitsRec(n: Int,initResult:Int,condStop:(Int)->Boolean,processFunc:(Int,Int)->Int):Int =
+        if(condStop(n)) initResult else processFunc(abs(n)%10,digitsRec(abs(n)/10,initResult,condStop,processFunc))
+
     //Рекурсия вверх для поиска макс цифры числа
-    fun maxDigitUp(n:Int):Int = if(n==0) 0 else max(abs(n)%10,maxDigitUp(abs(n)/10))
+    fun maxDigitUp(n:Int):Int = digitsRec(n,0,{k:Int->k==0},{digit:Int,prevResult:Int->max(digit,prevResult)})
     //Рекурсия вверх для поиска суммы цифр кратных 3
-    fun sum3DigitsUp(n: Int):Int = if(n==0) 0 else when{abs(n)%10%3==0->abs(n)%10;else->0}+sum3DigitsUp(abs(n)/10)
+    fun sum3DigitsUp(n: Int):Int = digitsRec(n,0,{k:Int->k==0},{digit:Int,prevResult:Int->if(digit%3==0)prevResult+digit else prevResult})
+
+
+    //Функция высшего порядка для обработки делителей
+    fun dividerProcessRec(n:Int, initResult: Int,currentDiv: Int, condStop: (Int, Int) -> Boolean, processFunc: (Int, Int, Int) -> Int):Int =
+        if(condStop(n,currentDiv))initResult else processFunc(n,currentDiv,dividerProcessRec(n,initResult,currentDiv+1,condStop,processFunc))
 
     //Рекурсия вверх для нахождения количества делителей целого числа
-    fun findDividersCountUpCall(n: Int):Int = if(n==0) 0 else findDividersCountUp(abs(n),1)
-    fun findDividersCountUp(n: Int,currentDiv:Int):Int = if(!(currentDiv<=(n/2).toInt())) 1 else
-        when{abs(n)%currentDiv==0->1;else->0}+findDividersCountUp(n,currentDiv+1);
+    fun findDividersCountUpCall(n: Int):Int = if(n==0) 0 else dividerProcessRec(abs(n),1,1,{k:Int,currentDiv:Int->!(currentDiv<=(k/2).toInt())},
+        {k:Int,currentDiv:Int,prevResult:Int->if(k%currentDiv==0) prevResult+1 else prevResult})
+
 
 }
