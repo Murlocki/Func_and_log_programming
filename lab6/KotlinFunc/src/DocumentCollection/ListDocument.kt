a10e916063ecf539f6495d132135c32da446b79b
Murlocki <kirillkornilov03@gmail.com>
2024-05-24 22:34:19 +0300

    Lab 6 task 8 Added ListDocument class

--- lab6/KotlinFunc/src/DocumentCollection/ListDocument.kt
+++ lab6/KotlinFunc/src/DocumentCollection/ListDocument.kt
@@ -1,4 +1,15 @@
 package DocumentCollection
 
-class ListDocument {
+import VehiclePassport.VehiclePassport
+
+class ListDocument(val documents: List<VehiclePassport>): DocumentCollection<VehiclePassport>() {
+    override fun searchDoc(doc: VehiclePassport): VehiclePassport? {
+        return searchList(doc, documents.size)
+    }
+    private fun searchList(doc: VehiclePassport,maxIndex:Int,currentIndex:Int=0):VehiclePassport? =
+        when{
+            (currentIndex >= maxIndex)->null;
+            (documents[currentIndex] == doc)->doc;
+            else->searchList(doc, maxIndex, currentIndex + 1)
+        }
 }
