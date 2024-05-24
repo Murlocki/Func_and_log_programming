92fd62d3246e9be41ea2ad72493870611de02891
Murlocki <kirillkornilov03@gmail.com>
2024-05-24 18:59:44 +0300

    Lab 6 task 4 Added function for sorting of vehicle passport by series-number and date

--- lab6/KotlinFunc/src/VehiclePassport.kt
+++ lab6/KotlinFunc/src/VehiclePassport.kt
@@ -1,9 +1,13 @@
 import java.time.LocalDate
 import java.util.Date
 
-class VehiclePassport(val series:Int, val number:Int, val issueDate: LocalDate) {
+class VehiclePassport(val series:Int, val number:Int, val issueDate: LocalDate):Comparable<VehiclePassport> {
     override fun toString(): String {
         return "Серия:$series,Номер:$number,Дата выдачи:$issueDate"
+    }
+
+    override fun compareTo(other: VehiclePassport): Int {
+        return this.issueDate.compareTo(other.issueDate)
     }
 
     override fun equals(other: Any?): Boolean =
@@ -13,9 +17,18 @@ class VehiclePassport(val series:Int, val number:Int, val issueDate: LocalDate) 
             else-> (compareBySeries(this,other)==0)&& (compareByNumber(this,other) == 0)
     }
 
+
+
     companion object{
         fun compareBySeries(p1:VehiclePassport,p2:VehiclePassport):Int = p1.series.compareTo(p2.series)
         fun compareByNumber(p1:VehiclePassport,p2:VehiclePassport):Int = p1.number.compareTo(p2.number)
 
+        fun compareBySeriesAndNumber(p1: VehiclePassport,p2: VehiclePassport):Int{
+            val seriesComp = compareBySeries(p1,p2)
+            if(seriesComp!=0){
+                return seriesComp
+            }
+            return compareByNumber(p1,p2)
+        }
     }
 }
--- lab6/KotlinFunc/src/VehiclePassportTests.kt
+++ lab6/KotlinFunc/src/VehiclePassportTests.kt
@@ -1,3 +1,4 @@
+import VehiclePassport.Companion.compareBySeriesAndNumber
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -8,6 +9,8 @@ internal class VehiclePassportTests {
         val passport = VehiclePassport(1234,233, LocalDate.of(2004,12,23))
         Assertions.assertEquals("Серия:1234,Номер:233,Дата выдачи:2004-12-23",passport.toString())
     }
+
+
     @Test
     fun compareEqualNumberTest(){
         val passport = VehiclePassport(1234,233, LocalDate.now())
@@ -21,6 +24,8 @@ internal class VehiclePassportTests {
         Assertions.assertEquals(-1,VehiclePassport.compareByNumber(passport,passport2))
         Assertions.assertEquals(1,VehiclePassport.compareByNumber(passport2,passport))
     }
+
+
     @Test
     fun compareEqualSeriesest(){
         val passport = VehiclePassport(123,233, LocalDate.now())
@@ -34,6 +39,8 @@ internal class VehiclePassportTests {
         Assertions.assertEquals(1,VehiclePassport.compareBySeries(passport,passport2))
         Assertions.assertEquals(-1,VehiclePassport.compareBySeries(passport2,passport))
     }
+
+
     @Test
     fun equalsNumberSeriesTest(){
         val passport = VehiclePassport(1234,23, LocalDate.now())
@@ -45,5 +52,39 @@ internal class VehiclePassportTests {
         val passport = VehiclePassport(123,23, LocalDate.now())
         val passport2 = VehiclePassport(123,23, LocalDate.now())
         Assertions.assertEquals(true, passport == passport2)
+    }
+
+
+    @Test
+    fun sortByDataTest(){
+        val passport = VehiclePassport(123,23, LocalDate.of(1234,12,23))
+        val passport2 = VehiclePassport(123,23, LocalDate.of(1200,12,23))
+        val passport3 = VehiclePassport(123,23, LocalDate.now())
+
+        val listToSort = listOf(passport,passport3,passport2)
+
+        Assertions.assertEquals(listOf(passport2,passport,passport3), listToSort.sorted())
+    }
+
+    @Test
+    fun sortBySeriesNumberTest(){
+        val passport = VehiclePassport(123,21, LocalDate.of(1234,12,23))
+        val passport2 = VehiclePassport(123,25, LocalDate.of(1200,12,23))
+        val passport3 = VehiclePassport(123,13, LocalDate.now())
+
+        val listToSort = listOf(passport,passport3,passport2)
+
+        Assertions.assertEquals(listOf(passport3,passport,passport2), listToSort.sortedWith(::compareBySeriesAndNumber) )
+    }
+
+    @Test
+    fun sortBySeriesNumberTest2(){
+        val passport = VehiclePassport(123,23, LocalDate.of(1234,12,23))
+        val passport2 = VehiclePassport(123,23, LocalDate.of(1200,12,23))
+        val passport3 = VehiclePassport(123,23, LocalDate.now())
+
+        val listToSort = listOf(passport,passport3,passport2)
+
+        Assertions.assertEquals(listOf(passport3,passport,passport2), listToSort.sortedWith(::compareBySeriesAndNumber) )
     }
 }
